533fa3cf1f63bb9ca5f05df95c1a561fa55eca48
kboisseleau <boisseleau.kevin@gmail.com>
2023-06-06 11:01:03 +0200

    init project

---
+++ test/unit/mock/repository.mock.ts
@@ -0,0 +1,19 @@
+export class RepositoryrMock {
+  static readonly mockRepo = {
+    save: jest.fn(),
+    update: jest.fn(),
+    find: jest.fn(),
+    findOne: jest.fn(),
+    insert: jest.fn(),
+    createQueryBuilder: jest.fn()
+  }
+
+  static readonly mockUserRepository = {
+    findAll: jest.fn(),
+    findOne: jest.fn(),
+    findOneEmail: jest.fn(),
+    remove: jest.fn(),
+    save: jest.fn()
+
+  }
+}
---
+++ test/unit/mock/user.mock.ts
@@ -0,0 +1,27 @@
+// import { User } from 'db/entities/User'
+
+// export class UserMock {
+//   static readonly users: User[] = [
+//     {
+//       id: 1,
+//       email:'boisseleau.kevin@gmail.com',
+//       firstname: 'kevin',
+//       lastname: 'boisseleau',
+//       password: 'test'
+//     },
+//     {
+//       id: 2,
+//       email: 'john.doe@gmail.com',
+//       lastname: 'john',
+//       firstname: 'doe',
+//       password: 'password'
+//     }
+//   ]
+
+//   static readonly userSignUp: Partial<User> = {
+//     email: 'john.doe@gmail.com',
+//     lastname: 'john',
+//     firstname: 'doe',
+//     password: 'password'
+//   }
+// }
